e91d3f409f48387223bed54021db152bd1661b45
Fran7749 <trailrunner364@gmail.com>
2026-05-04 11:27:38 +0100

    Create service worker file sw.js for offline functionality and caching

---
+++ sw.js
@@ -0,0 +1,50 @@
+// This service worker caches static assets for offline functionality.
+
+const CACHE_NAME = 'my-site-cache-v1';
+const urlsToCache = [
+  '/index.html',
+  '/styles/main.css',
+  '/script/main.js',
+  // Add any other assets you want to cache
+];
+
+// Install the service worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch assets from the cache
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+// Update the cache version
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
